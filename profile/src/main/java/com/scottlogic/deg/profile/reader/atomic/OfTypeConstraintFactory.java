de6ef9f4b46d8ffc7aae1cefd8932d962578e3a6
Tom <thayden@scottlogic.com>
2019-10-14 11:11:33 +0100

    Added date to schema and gave it a granularity of days

--- profile/src/main/java/com/scottlogic/deg/profile/reader/atomic/OfTypeConstraintFactory.java
+++ profile/src/main/java/com/scottlogic/deg/profile/reader/atomic/OfTypeConstraintFactory.java
@@ -2,12 +2,14 @@ package com.scottlogic.deg.profile.reader.atomic;
 
 import com.scottlogic.deg.common.profile.Field;
 import com.scottlogic.deg.common.profile.SpecificFieldType;
+import com.scottlogic.deg.common.profile.constraintdetail.DateTimeGranularity;
 import com.scottlogic.deg.common.profile.constraintdetail.NumericGranularityFactory;
 import com.scottlogic.deg.generator.profile.constraints.Constraint;
 import com.scottlogic.deg.generator.profile.constraints.atomic.*;
 import com.scottlogic.deg.profile.reader.file.names.NameRetriever;
 
 import java.math.BigDecimal;
+import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 
 public class OfTypeConstraintFactory {
@@ -25,6 +27,8 @@ public class OfTypeConstraintFactory {
             case LAST_NAME:
             case FULL_NAME:
                 return Optional.of(new IsInSetConstraint(field, NameRetriever.loadNamesFromFile(NameConstraintTypes.lookupProfileText(type.toLowerCase()))));
+            case DATE:
+                return Optional.of(new IsGranularToDateConstraint(field, new DateTimeGranularity(ChronoUnit.DAYS)));
             default:
                 return Optional.empty();
         }
